007bd95fe1c085f9ab6a6a065f64d35ad1e0062f
Jiwon <awesomedev3@rockwonitglobal.com>
2023-11-26 00:10:54 +0900

    #5.1 Rendering Templates

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "github.com/nomadcoders/nomadcoin/blockchain"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/nomadcoders/nomadcoin/blockchain"
+)
+
+const port string = ":4000"
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	tmpl.Execute(rw, data)
+}
 
 func main() {
-	chain := blockchain.GetBlockChain()
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+
 }
