8919a650e599caba24815412b3134c9e0608301b
Jiwon <awesomedev3@rockwonitglobal.com>
2023-12-01 21:40:48 +0900

    #6.2 MarshalText

--- main.go
+++ main.go
@@ -9,22 +9,33 @@ import (
 
 const port string = ":4000"
 
+type URL string
+
+func (u URL) MarshalText() (text []byte, err error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
 type URLDescription struct {
-	URL         string `json:"url"`
+	URL         URL    `json:"url"`
 	Method      string `json:"method"`
 	Description string `json:"description"`
 	Payload     string `json:"payload"`
 }
 
+func (u URLDescription) String() string {
+	return "Hello I'm the URL Description"
+}
+
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
 		{
-			URL:         "/",
+			URL:         URL("/"),
 			Method:      "GET",
 			Description: "See Documentation",
 		},
 		{
-			URL:         "/blocks",
+			URL:         URL("/blocks"),
 			Method:      "POST",
 			Description: "Add A Block",
 			Payload:     "data:string",
@@ -39,6 +50,7 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
 	http.HandleFunc("/", documentation)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
