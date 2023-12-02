eafe989e0ad59afa0a08adbe1f5aaa653eb89570
Jiwon <awesomedev3@rockwonitglobal.com>
2023-12-02 17:25:31 +0900

    #7.2 FlagSet

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -20,12 +21,23 @@ func main() {
 
 	}
 
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+
+	portFlag := rest.Int("port", 4000, "Sets the port of the server (default 4000)")
+
 	switch os.Args[1] {
 	case "explorer":
 		fmt.Println("Start Explorer")
 	case "rest":
-		fmt.Println("Start REST API")
+		rest.Parse(os.Args[2:])
 	default:
 		usage()
 	}
+
+	if rest.Parsed() {
+		fmt.Println(portFlag)
+		fmt.Println("Start server")
+	}
+
+	fmt.Println(*portFlag)
 }
